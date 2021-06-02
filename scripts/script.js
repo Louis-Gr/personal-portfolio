2c847187fd7f238af38558e5266b2c2c5f08634a
Louis-Gr <80121656+Louis-Gr@users.noreply.github.com>
2021-06-02 08:24:34 +0200

    JavaScript Challenge Details

---
+++ scripts/script.js
@@ -0,0 +1,22 @@
+let element = document.getElementById('social-media-detox-challenge');
+element.onclick = changeStyle;
+
+function changeStyle(){
+    if(element.style.height === "25rem"){
+        element.style.height = '15rem';
+        element.style.width = '12rem';
+        element.style.position = "relative"
+        element.style.transition = "0.5s ease"
+        document.getElementById("description-social-media-detox").style.visibility = "hidden"
+    }
+    else{
+        element.style.height = '25rem';
+        element.style.width = '20rem';
+        element.style.transition = "0.5s ease"
+        element.style.position = "absolute"
+        element.style.right = "auto"
+        element.style.left = "auto"
+        document.getElementById("description-social-media-detox").style.visibility = "visible"
+    }
+};
+
