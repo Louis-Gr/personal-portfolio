205cbf4b98467b0523c14a057ef499691d9e4868
Louis-Gr <80121656+Louis-Gr@users.noreply.github.com>
2021-06-03 12:35:44 +0200

    Javascript open and close ALL Challenges

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,26 @@
-let element = document.getElementById('social-media-detox-challenge');
+/*let element = document.getElementById('social-media-detox-challenge');
 element.onclick = changeStyle;
 element.onmouseover = function(){element.style.backgroundColor = "var(--col-green-4)"};
 element.onmouseleave = function(){element.style.backgroundColor = "var(--col-green-5)"}
-
-function changeStyle(){
+*/
+function changeStyle(element){
     if(element.style.height === "25rem"){
         element.style.height = '15rem';
         element.style.width = '12rem';
         element.style.position = "relative"
         element.style.transition = "0.5s ease"
-        document.getElementById("description-social-media-detox").style.visibility = "hidden"
+        if(element.id == "social-media-detox-challenge"){
+            document.getElementById("description-social-media-detox").style.visibility = "hidden"
+        }
+        else if (element.id == "meditation-challenge"){
+            document.getElementById("description-meditation").style.visibility = "hidden"
+        }
+        else if (element.id == "cold-shower-challenge"){
+            document.getElementById("description-cold-showers").style.visibility = "hidden"
+        }
+        else if (element.id == "ollie-challenge"){
+            document.getElementById("description-ollie").style.visibility = "hidden"
+        }
         element.style.margin = "0"
     }
     else{
@@ -17,6 +28,25 @@ function changeStyle(){
         element.style.width = '20rem';
         element.style.transition = "0.5s ease"
         element.style.margin = "0 25%"
-        document.getElementById("description-social-media-detox").style.visibility = "visible"
+        if(element.id == "social-media-detox-challenge"){
+            document.getElementById("description-social-media-detox").style.visibility = "visible"
+        }
+        else if (element.id == "meditation-challenge"){
+            document.getElementById("description-meditation").style.visibility = "visible"
+        }
+        else if (element.id == "cold-shower-challenge"){
+            document.getElementById("description-cold-showers").style.visibility = "visible"
+        }
+        else if (element.id == "ollie-challenge"){
+            document.getElementById("description-ollie").style.visibility = "visible"
+        }
     }
 };
+
+function hoverMouse(challenge){
+    challenge.style.backgroundColor = "var(--col-green-4)"
+}
+
+function mouseLeave(challenge){
+    challenge.style.backgroundColor = "var(--col-green-5)"};
+
