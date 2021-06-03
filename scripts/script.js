e4feeca71d91de3710f00befe93088bdfba1b40c
Louis-Gr <80121656+Louis-Gr@users.noreply.github.com>
2021-06-03 19:45:30 +0200

    Version added & Desktop Version done

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,12 @@
 
 function changeStyle(element){
-    if(element.style.height === "25rem"){
-        element.style.height = '15rem';
-        element.style.width = '12rem';
+    if(element.style.height === "48rem"){
+        element.style.height = '25rem';
+        element.style.width = '22rem';
         element.style.position = "relative"
         element.style.transition = "0.5s ease"
+        document.getElementById("challenges").scrollIntoView({behavior: 'smooth'});
+        element.style.margin = "0"
         if(element.id == "social-media-detox-challenge"){
             document.getElementById("description-social-media-detox").style.visibility = "hidden"
             document.getElementById("meditation-challenge").style.display = "block"
@@ -29,32 +31,35 @@ function changeStyle(element){
             document.getElementById("meditation-challenge").style.display = "block"
             document.getElementById("cold-shower-challenge").style.display = "block"
         }
-        element.style.margin = "0"
     }
     else{
-        element.style.height = '25rem';
-        element.style.width = '20rem';
+        element.style.height = '48rem';
+        element.style.width = '60rem';
         element.style.transition = "0.5s ease"
         element.style.margin = "0 auto"
         if(element.id == "social-media-detox-challenge"){
+            document.getElementById("social-media-detox-challenge").scrollIntoView({behavior: 'smooth'});
             document.getElementById("description-social-media-detox").style.visibility = "visible"
             document.getElementById("meditation-challenge").style.display = "none"
             document.getElementById("cold-shower-challenge").style.display = "none"
             document.getElementById("ollie-challenge").style.display = "none"
         }
         else if (element.id == "meditation-challenge"){
+            document.getElementById("meditation-challenge").scrollIntoView({behavior: 'smooth'});
             document.getElementById("description-meditation").style.visibility = "visible"
             document.getElementById("social-media-detox-challenge").style.display = "none"
             document.getElementById("cold-shower-challenge").style.display = "none"
             document.getElementById("ollie-challenge").style.display = "none"
         }
         else if (element.id == "cold-shower-challenge"){
+            document.getElementById("cold-shower-challenge").scrollIntoView({behavior: 'smooth'});
             document.getElementById("description-cold-showers").style.visibility = "visible"
             document.getElementById("social-media-detox-challenge").style.display = "none"
             document.getElementById("meditation-challenge").style.display = "none"
             document.getElementById("ollie-challenge").style.display = "none"
         }
         else if (element.id == "ollie-challenge"){
+            document.getElementById("meditation-challenge").scrollIntoView({behavior: 'smooth'});
             document.getElementById("description-ollie").style.visibility = "visible"
             document.getElementById("social-media-detox-challenge").style.display = "none"
             document.getElementById("meditation-challenge").style.display = "none"
